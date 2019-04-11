7e52ae9872c351d60271bcc06666bf1c65ef80dc
BrysonWilks <wilksbryson@gmail.com>
2019-04-11 18:36:41 -0400

    updated PersonalUI

--- src/pkg412proj/PersonalUIController.java
+++ src/pkg412proj/PersonalUIController.java
@@ -5,9 +5,19 @@
  */
 package pkg412proj;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,6 +28,10 @@ public class PersonalUIController implements Initializable {
     
     
     private User user; //The logged in user
+    private Stage stage;
+    private Parent root;
+    @FXML
+    private Button backButton; //Refers to the back button in the view
 
     /**
      * Initializes the controller class.
@@ -49,4 +63,21 @@ public class PersonalUIController implements Initializable {
         this.user = user;
     }
     
+     public void goHome() {
+        URL url = null;
+        try {
+            url = new File("src/pkg412proj/NavUI.fxml").toURI().toURL();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(NavUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        stage = (Stage) backButton.getScene().getWindow();
+        try {
+            root = FXMLLoader.load(url);
+        } catch (IOException ex) {
+        }
+        NavigationManager.getInstance(stage).showScene(root);
+    }
+
+
 }
